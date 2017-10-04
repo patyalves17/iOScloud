7bf1152bfcb8800b6758bdc578382c0f66b75f93
Usuário Convidado <Guest@PA06MICRO14.local>
2017-10-04 19:31:00 -0300

    inicial

---
+++ ioscloudaula01/Cars/CarsTableViewController.swift
@@ -0,0 +1,37 @@
+//
+//  CarsTableViewController.swift
+//  Cars
+//
+//  Created by Eric.
+//  Copyright © 2017 EricBrito. All rights reserved.
+//
+
+import UIKit
+
+class CarsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        return cell
+    }
+
+}
+
+
+
+
+
---
+++ ioscloudaula01/Cars/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Cars
+//
+//  Created by Eric.
+//  Copyright © 2017 EricBrito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tfBrand: UITextField!
+    @IBOutlet weak var tfName: UITextField!
+    @IBOutlet weak var tfPrice: UITextField!
+    @IBOutlet weak var scGasType: UISegmentedControl!
+    @IBOutlet weak var ivCars: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func saveCar(_ sender: UIButton) {
+    }
+}
+
+
+
